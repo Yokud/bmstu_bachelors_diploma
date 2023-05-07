d7b9f8dea39a3c7c12a21443277a49e7a2743698
Ivan Malyshev <thefuny@mail.ru>
2023-05-07 03:49:30 +0300

    add image filtering

--- src/Assets/Scripts/LightPosCalc.cs
+++ src/Assets/Scripts/LightPosCalc.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 
 public class LightPosCalc : MonoBehaviour
 {
+    const double MinPixelsCoveragePercent = 0.98;
+
     public Button StartButton;
     public GameObject EnvironmentData;
     public GameObject LightCoordsReceiver;
@@ -27,12 +29,36 @@ public class LightPosCalc : MonoBehaviour
         }
 
         var grayscaled = new Mat();
-        Cv2.CvtColor(env.SpherePano, grayscaled, ColorConversionCodes.RGBA2GRAY);
+        Cv2.CvtColor(env.SpherePano, grayscaled, ColorConversionCodes.BGRA2GRAY);
 
-        //var tex = OpenCvSharp.Unity.MatToTexture(grayscaled);
-        //var bytes = tex.EncodeToPNG();
-        //File.WriteAllBytes(@"D:\gray.png", bytes);
+        grayscaled.MinMaxLoc(out double minGray, out double maxGray);
+        var avgGray = grayscaled.Mean(grayscaled).Val0;
 
+        if (maxGray / avgGray < 1.5)
+        {
+            Debug.Log("Can't calculate light source positions, because the sphere map has no observable difference between point light and ambient light");
+            return;
+        }
 
+        // Histogram analysis
+        var hist = new Mat();
+        Cv2.CalcHist(new[] { grayscaled }, new[] { 0 }, null, hist, 1, new[] { 256 }, new[] { new Rangef(0, 256) });
+        var sumPixels = (double)grayscaled.Width * grayscaled.Height;
+
+        var brightnessVal = 255;
+        while (hist.RowRange(brightnessVal, 256).Sum().Val0 / sumPixels < 1 - MinPixelsCoveragePercent)
+            brightnessVal--;
+
+        var thresholded = new Mat();
+        Cv2.Threshold(grayscaled, thresholded, brightnessVal - 1, 255, ThresholdTypes.Tozero); // Затемняем пиксели, яркость которых меньше brightnessVal
+        var filtered = new Mat();
+        Cv2.MedianBlur(thresholded, filtered, 7);
+    }
+
+    void SavePng(Mat image)
+    {
+        var tex = OpenCvSharp.Unity.MatToTexture(image);
+        var bytes = tex.EncodeToPNG();
+        File.WriteAllBytes(@"D:\filtered.png", bytes);
     }
 }
